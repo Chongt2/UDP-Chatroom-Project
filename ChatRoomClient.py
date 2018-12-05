6bc42067744b23c38272450f22f9fd527a1c0d18
Tai-Yin Chong <t.m.chong16@gmail.com>
2018-12-04 21:01:17 -0500

    Add files via upload

--- ChatRoomClient.py
+++ ChatRoomClient.py
@@ -1,10 +1,24 @@
 from socket import *
+import threading
+import time
 
+
+def listener():
+    #May need to set a parameter to  so that when the user wants to quit the thread can know when to send
+    #a goodbye message
+    while True:
+        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+        print(modifiedMessage.decode())
+        time.sleep(0.300)
+
+# def sender():
+#     while True:
+        
 serverName = 'localhost'
 serverPort = 5000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = input("Type \"Join\" to join the chat room: ")
-if message == "Join":
+if message.lower().find("join") >= 0:
     clientSocket.sendto(message.encode(), (serverName, serverPort))
     userName = input("What is your user name? ")
     #lines 11 and 12 require multithreading. Clients cannot receive a message
@@ -12,15 +26,18 @@ if message == "Join":
     clientSocket.sendto(userName.encode(), (serverName, serverPort))
     welcomeMessage, serverAddress = clientSocket.recvfrom(2048)
     modifiedWelcomeMessage = welcomeMessage.decode()
-    print(modifiedWelcomeMessage + "!")
-    message = input(userName + ": ")
-    while message != "Quit": 
-        #thread 1
+    print(modifiedWelcomeMessage)
+    #After client joins the listener thread runs immediately.
+    t1 = threading.Thread(target=listener)
+    t1.start()
+    #Without formatting the incoming message is received after the receiving clients user name
+    #e.g.: If Chris sent "HI", Jon would see "Jon: Chris: HI
+    #message = input(userName + ": ")
+    message = input()
+    while message.lower().find('quit') < 0: 
         clientSocket.sendto(message.encode(), (serverName, serverPort))
-        #end thread 1
-        #thread 2
-        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-        print(modifiedMessage.decode())
-        message = input(userName + ": ")
-        #end thread 2
-    clientSocket.close()
+        message = input()
+
+    clientSocket.sendto(message.encode(), (serverName, serverPort))
+    if not t1.isAlive():
+        clientSocket.close()
--- ChatRoomServer.py
+++ ChatRoomServer.py
@@ -12,7 +12,7 @@ while True:
     message, clientAddress = serverSocket.recvfrom(2048)
     print(clientAddress)
     decodedMessage = message.decode()
-    if decodedMessage == "Join":
+    if decodedMessage.lower().find("join") >= 0:
         ipArray.append(clientAddress)
         userName, clientAddress = serverSocket.recvfrom(2048)
         decodedUserName = userName.decode()
@@ -24,17 +24,17 @@ while True:
             for x in range(0, len(userArray)):
                 print("send to " + userArray[x])
                 serverSocket.sendto(welcomeMessage.encode(), ipArray[x])
-    elif decodedMessage == "Quit":
+    elif decodedMessage.lower().find("quit") >= 0:
         for x in range(0, len(userArray)):
             if clientAddress == ipArray[x]:
                 goodbyeMessage = userArray[x] + "has left the chat room."
                 for y in range(0, len(userArray)):
                     serverSocket.sendto(goodbyeMessage.encode(), ipArray[y])
     else:
+        print("OOPS")
         for x in range(0, len(userArray)):
             if clientAddress == ipArray[x]:
                 modifiedMessage = userArray[x] + ": " + decodedMessage
-#         modifiedMessage = time + " " + decodedMessage
         print("message to be sent back: ")
         print(modifiedMessage)
         if len(userArray) == 1:
